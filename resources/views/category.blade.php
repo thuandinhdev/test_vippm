a48da69ca0f0024beea49f93260ee1f62db3f00c
thuandinhdev <thuandinhdev@gmail.com>
2022-06-22 15:46:14 +0700

    update

---
+++ resources/views/category.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    
+<head>
+        <meta charset="utf-8" />
+        <title>Landrick - Saas & Software Landing Page Template</title>
+        <!-- favicon -->
+        <link rel="shortcut icon" href="{{$resource_path}}images/favicon.ico">
+        <!-- Bootstrap -->
+        <link href="{{$resource_path}}css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <!-- Icons -->
+        <link href="{{$resource_path}}css/materialdesignicons.min.css" rel="stylesheet" type="text/css" />
+        <!-- Slider -->               
+        <link rel="stylesheet" href="{{$resource_path}}css/owl.carousel.min.css"/> 
+        <link rel="stylesheet" href="{{$resource_path}}css/owl.theme.default.min.css"/>    
+        <!-- Main Css -->
+        <link href="{{$resource_path}}css/style.css" rel="stylesheet" type="text/css" id="theme-opt" />
+        <link href="{{$resource_path}}css/colors/default.css" rel="stylesheet" id="color-opt">
+
+    </head>
+
+    <body>
+        <header id="topnav" class="defaultscroll sticky">
+    <div class="container">
+        
+    </div>
+</header>
+    <!-- Start Style switcher -->
+    <div id="style-switcher" style="left: 0px;" class="bg-light border p-3 pt-2 pb-2"  resource_path="{{$resource_path}}">
+        <h3 class="title text-center">Select Your Color</h3>
+        <ul class="pattern text-center mb-2">
+            <li class="list-inline-item">
+                <a class="default" href="#"></a>
+            </li>
+            <li class="list-inline-item">
+                <a class="green" href="#"></a>
+            </li>
+            <li class="list-inline-item">
+                <a class="purple" href="#"></a>
+            </li>
+            <li class="list-inline-item">
+                <a class="red" href="#"></a>
+            </li>
+            <li class="list-inline-item">
+                <a class="skyblue" href="#"></a>
+            </li>
+            <li class="list-inline-item">
+                <a class="skobleoff" href="#"></a>
+            </li>
+            <li class="list-inline-item">
+                <a class="cyan" href="#"></a>
+            </li>
+            <li class="list-inline-item">
+                <a class="slateblue" href="#"></a>
+            </li>
+        </ul>
+
+        <h3 class="title text-center pt-3 mb-0 border-top">Theme Option</h3>
+        <div class="text-center">
+            <a href="#" class="btn btn-sm w-100 btn-primary rtl-version t-rtl-light mt-2">RTL</a>
+            <a href="#" class="btn btn-sm w-100 btn-primary ltr-version t-ltr-light mt-2">LTR</a>
+            <a href="#" class="btn btn-sm w-100 btn-primary dark-rtl-version t-rtl-dark mt-2">RTL</a>
+            <a href="#" class="btn btn-sm w-100 btn-primary dark-ltr-version t-ltr-dark mt-2">LTR</a>
+            <a href="#" class="btn btn-sm w-100 btn-dark dark-version t-dark mt-2">Dark</a>
+            <a href="#" class="btn btn-sm w-100 btn-dark light-version t-light mt-2">Light</a>
+        </div>
+        <div class="bottom">
+            <a href="#" class="settings bg-white shadow d-block"><i class="mdi mdi-cog ml-1 mdi-24px position-absolute mdi-spin text-primary"></i></a>
+        </div>
+    </div>
+    <!-- End Style switcher -->
+
+    <!-- Back to top -->
+    <a href="#" class="btn btn-icon btn-soft-primary back-to-top"><i data-feather="arrow-up" class="icons"></i></a>
+    <!-- Back to top -->
+
+    <!-- javascript -->
+    <script src="{{$resource_path}}js/jquery-3.5.1.min.js"></script>
+    <script src="{{$resource_path}}js/bootstrap.bundle.min.js"></script>
+    <script src="{{$resource_path}}js/jquery.easing.min.js"></script>
+    <script src="{{$resource_path}}js/scrollspy.min.js"></script>
+    <!-- SLIDER -->
+    <script src="{{$resource_path}}js/owl.carousel.min.js"></script>
+    <script src="{{$resource_path}}js/owl.init.js"></script>
+    <!-- Icons -->
+    <script src="{{$resource_path}}js/feather.min.js"></script>
+    <!-- Switcher -->
+    <script src="{{$resource_path}}js/switcher.js"></script>
+    <!-- Main Js -->
+    <script src="{{$resource_path}}js/app.js"></script>
+</body>
+
+</html>
