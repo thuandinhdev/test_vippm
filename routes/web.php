12897dfeb088093d9e2ed53a2d533e164d322917
thuandinhdev <thuandinhdev@gmail.com>
2022-06-30 22:44:56 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -10,21 +10,27 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+$path_string = str_replace([str_replace(['https://', 'http://'], '', env('APP_URL')), str_replace(['https://', 'http://'], '', env('FRONT_URL'))], '', str_replace(['https://', 'http://'], '', URL::current()));
 
-$path_string = str_replace(str_replace(['https://', 'http://'], '', env('APP_URL')), '', str_replace(['https://', 'http://'], '', URL::current()));
-if(explode('/', $path_string)[0] == ''){
-    return redirect(config('app.front_url').'/home');
+if(in_array(explode('/', $path_string)[0], ['index.html', 'index.php'])){
+    Route::get($path_string, function () {
+        return redirect(env('FRONT_URL').'/home');
+    });
+} else {
+    if(explode('/', $path_string)[0] != 'api'){
+        Route::get($path_string, 'FontendController@controlPage');    
+    }
 }
-if(explode('/', $path_string)[0] != 'api' && explode('/', $path_string)[0] != 'admin'){
-    Route::get($path_string, 'FontendController@controlPage');    
-}
+
 
 Route::get('/', function () {
+    return redirect(env('FRONT_URL').'/home');
     if (!is_null(config('app.front_url'))) {
         return redirect(config('app.front_url'));
     }
     return response()->json('Success');
 })->name('index');
+
 
 // Application cache cleared.
 Route::get('/cache-clear', function() {
@@ -41,5 +47,6 @@ Route::get('/config-cache', function() {
 // Configuration cached.
 Route::get('/queue-work', function() {
     $r = Artisan::call('queue:work');
+    pr($r);
     return "Done!";
 });
